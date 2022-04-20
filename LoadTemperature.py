0df7fc97bf4dc6855e33e596e021045cf73ee4eb
unknown <davecon88@gmail.com>
2022-04-21 00:22:23 +0930

    error is accessing postgresql

--- LoadTemperature.py
+++ LoadTemperature.py
@@ -17,25 +17,16 @@ app = Flask(__name__)
 
 @app.route("/temperature")
 def return_temperature():
-    try:
-        sql = "select time, temperature from TemperatureRecords "
-        print("started")
-        print(dbCursor)
-        dbCursor.execute(sql)
-        print("Execution complete")
-        temperatureData = dbCursor.fetchall()
-        print("fetchall complete")
-        temperatureDict = dict( time = [], temperature =[])
-        for row in temperatureData:
-            
-            temperatureDict["time"].append((row['time']).strftime("%x %X"))
-            temperatureDict["temperature"].append(row['temperature'])
-        fig = create_svg(temperatureDict)
-        return fig
-    except Exception as e:
-        print(repr(e))
-        return("exception")
-
+    sql = "select time, temperature from TemperatureRecords where id < %s"
+    dbCursor.execute(sql, (700000,))
+    temperatureData = dbCursor.fetchall()
+    temperatureDict = dict( time = [], temperature =[])
+    for row in temperatureData:
+        
+        temperatureDict["time"].append((row['time']).strftime("%x %X"))
+        temperatureDict["temperature"].append(row['temperature'])
+    fig = create_svg(temperatureDict)
+    return fig
 
 def create_svg(df):
     fig = px.line(df, x="time", y="temperature", title='soil temp')
@@ -44,13 +35,38 @@ def create_svg(df):
     img_bytes = fig.to_image(format="svg", width=1500, height=1000)
     return img_bytes
 
+def get_temperature():
+    while(True):
+        try: 
+            url = "https://b324-27-33-189-105.ngrok.io/temperature"
+            r = requests.get(url)
+            tempData = json.loads(r.content.decode('UTF-8'))
+            temperature = tempData.get('value')
+            
+            date = datetime.now()
+        
+
+            sql = '''insert into TemperatureRecords(time, temperature)
+            VALUES(%s, %s);'''
+            dbCursor.execute(sql,(date, temperature))
+
+
+            print(tempData.get('value'))
+        except Exception as e:
+            print(e) 
+
+        db.conn.commit()
+        db.conn.close()
+        time.sleep(10)
 if __name__ == '__main__':
 
- 
+    p = Process(target=get_temperature)
+    p.start()
+
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host='0.0.0.0')
+    app.run(threaded = True, debug=True, use_reloader=False)
 
 
 
